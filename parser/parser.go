5700ce84e9661d230e59ea785cc103268ec397e1
MichaelBo1 <local@local.net>
2025-08-31 14:05:10 +0100

    Parsing identifiers and int literals

--- parser/parser.go
+++ parser/parser.go
@@ -2,10 +2,29 @@ package parser
 
 import (
 	"fmt"
+	"strconv"
 
 	"github.com/MichaelBo1/go_interpreter/ast"
 	"github.com/MichaelBo1/go_interpreter/lexer"
 	"github.com/MichaelBo1/go_interpreter/token"
+)
+
+type OperatorPrecedence int
+
+const (
+	_ OperatorPrecedence = iota
+	LOWEST
+	EQUALS      // ==
+	LESSGREATER // < or <= or > or >=
+	SUM         // +
+	PRODUCT     // *
+	PREFIX      // -x or !x
+	CALL        // function(x)
+)
+
+type (
+	prefixParseFn func() ast.Expression
+	infixParseFn  func(ast.Expression) ast.Expression
 )
 
 type Parser struct {
@@ -13,10 +32,17 @@ type Parser struct {
 	currentToken token.Token
 	peekToken    token.Token
 	errors       []string
+
+	prefixParseFns map[token.TokenType]prefixParseFn
+	infixParseFns  map[token.TokenType]infixParseFn
 }
 
 func New(lex *lexer.Lexer) *Parser {
 	parser := &Parser{lex: lex}
+
+	parser.prefixParseFns = make(map[token.TokenType]prefixParseFn)
+	parser.registerPrefix(token.IDENTIFIER, parser.parseIdentifier)
+	parser.registerPrefix(token.INT, parser.parseIntegerLiteral)
 	// We read two tokens ahead so curToken and peekToken are set by
 	// lexing two tokens. If the input to the lexer is empty, we will
 	// check and see that the curToken is a token.EOF and don't worry about the peekToken in that case.
@@ -55,7 +81,7 @@ func (p *Parser) parseStatement() ast.Statement {
 	case token.RETURN:
 		return p.parseReturnStatement()
 	default:
-		return nil
+		return p.parseExpressionStament()
 	}
 }
 
@@ -93,9 +119,51 @@ func (p *Parser) parseReturnStatement() *ast.ReturnStatement {
 	return stmt
 }
 
+func (p *Parser) parseExpressionStament() *ast.ExpressionStatement {
+	stmt := &ast.ExpressionStatement{Token: p.currentToken}
+
+	stmt.Expression = p.parseExpression(LOWEST)
+
+	if p.peekTokenIs(token.SEMICOLON) {
+		p.NextToken()
+	}
+	return stmt
+}
+
+func (p *Parser) parseExpression(precedence OperatorPrecedence) ast.Expression {
+	prefix := p.prefixParseFns[p.currentToken.Type]
+	if prefix == nil {
+		return nil
+	}
+	leftExp := prefix()
+	return leftExp
+}
+
+func (p *Parser) parseIdentifier() ast.Expression {
+	return &ast.Identifier{Token: p.currentToken, Value: p.currentToken.Literal}
+}
+
+func (p *Parser) parseIntegerLiteral() ast.Expression {
+	intLit := &ast.IntegerLiteral{Token: p.currentToken}
+
+	value, err := strconv.ParseInt(p.currentToken.Literal, 0, 64)
+	if err != nil {
+		msg := fmt.Sprintf("could not parse %q as integer", p.currentToken.Literal)
+		p.errors = append(p.errors, msg)
+		return nil
+	}
+
+	intLit.Value = value
+	return intLit
+}
+
 func (p *Parser) peekError(expectedType token.TokenType) {
 	msg := fmt.Sprintf("expected next token to be %s, got %s.", expectedType, p.peekToken.Type)
 	p.errors = append(p.errors, msg)
+}
+
+func (p *Parser) peekTokenIs(expectedType token.TokenType) bool {
+	return p.peekToken.Type == expectedType
 }
 
 func (p *Parser) expectPeek(expectedType token.TokenType) bool {
@@ -105,4 +173,12 @@ func (p *Parser) expectPeek(expectedType token.TokenType) bool {
 	}
 	p.peekError(expectedType)
 	return false
+}
+
+func (p *Parser) registerPrefix(tokenType token.TokenType, fn prefixParseFn) {
+	p.prefixParseFns[tokenType] = fn
+}
+
+func (p *Parser) registerInfix(tokenType token.TokenType, fn infixParseFn) {
+	p.infixParseFns[tokenType] = fn
 }
--- parser/parser_test.go
+++ parser/parser_test.go
@@ -72,6 +72,77 @@ func TestParsesReturnStatments(t *testing.T) {
 	}
 }
 
+func TestParsesIdentifierExpression(t *testing.T) {
+	input := "foobar;"
+
+	lex := lexer.New(input)
+	par := New(lex)
+	program := par.ParseProgram()
+
+	if program == nil {
+		t.Fatalf("ParseProgram() returned nil")
+	}
+	checkParserErrors(t, par)
+
+	if len(program.Statements) != 1 {
+		t.Fatalf("program.Statements does not contain 1 statement. Got %d statements", len(program.Statements))
+	}
+
+	stmt, ok := program.Statements[0].(*ast.ExpressionStatement)
+	if !ok {
+		t.Fatalf("program.Statements[0] is not an ExpressionStatementm got %T", stmt)
+	}
+
+	ident, ok := stmt.Expression.(*ast.Identifier)
+	if !ok {
+		t.Fatalf("exp not *ast.Identifier, got %T", ident)
+	}
+
+	if ident.Value != "foobar" {
+		t.Errorf("ident.Value not %s. got=%s", "foobar", ident.Value)
+	}
+	if ident.TokenLiteral() != "foobar" {
+		t.Errorf("ident.TokenLiteral not %s. got=%s", "foobar", ident.TokenLiteral())
+	}
+}
+
+func TestIntegerLiteralExpression(t *testing.T) {
+	input := "5;"
+	l := lexer.New(input)
+	p := New(l)
+
+	program := p.ParseProgram()
+	if program == nil {
+		t.Fatalf("ParseProgram() returned nil")
+	}
+	checkParserErrors(t, p)
+
+	if len(program.Statements) != 1 {
+		t.Fatalf("program has not enough statements. got=%d",
+			len(program.Statements))
+	}
+
+	stmt, ok := program.Statements[0].(*ast.ExpressionStatement)
+	if !ok {
+		t.Fatalf("program.Statements[0] is not ast.ExpressionStatement. got=%T",
+			program.Statements[0])
+	}
+
+	literal, ok := stmt.Expression.(*ast.IntegerLiteral)
+	if !ok {
+		t.Fatalf("exp not *ast.IntegerLiteral. got=%T", stmt.Expression)
+	}
+
+	if literal.Value != 5 {
+		t.Errorf("literal.Value not %d. got=%d", 5, literal.Value)
+	}
+
+	if literal.TokenLiteral() != "5" {
+		t.Errorf("literal.TokenLiteral not %s. got=%s", "5",
+			literal.TokenLiteral())
+	}
+}
+
 func testLetStatement(t testing.TB, parsedStmt ast.Statement, expectedName string) {
 	t.Helper()
 
